ffadb01855c7bcda2c2838e622710191755287a0
zeinab313 <152908582+zeinab313@users.noreply.github.com>
2023-12-10 03:55:03 -0800

    putting up word
    create a dictionary in view and putting up amounts dynamic instead static

--- cvsite/views.py
+++ cvsite/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index_view(request):
-    context={'name':'zeinab','family':'ghazaei'}
+    context={'name':'zeinab','family':'ghazaei','brith':'1368/5/25','eduction':'Bachelor of Information Technology',
+             'expertise':'Familiarity with C# and PHP programming languages,I did my thesis in PHP and I just started Python and I am currently learning Django.',
+             'email':'zghazaei313@gmail.com',
+             'phone':'0919*****45'
+             }
     return render(request,'cvsite/index.html',context)
